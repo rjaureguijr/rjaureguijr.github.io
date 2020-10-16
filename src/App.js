0dfa3ad65d2872a1c3e51f6313f1ec4fad733cab
Rudy Jauregui <rjaureguijr@gmail.com>
2020-10-16 12:41:53 -0500

    Edits for site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch
+} from "react-router-dom";
+import About from './About';
+import Projects from './Projects';
+import { Contact } from './Contact';
+import { NavBar } from './Nav';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/about" component={About}>
+            {/* <About /> */}
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/projects" component={Projects}>
+            {/* <Projects /> */}
+          </Route>
+          <Route path="/contact" component={Contact}>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Rudy Jauregui</h1>
+      <code>Website of Front End Developer | UI Engineer rjaureguijr</code>
+  </div>
+  );
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+    <div className="container">
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected 
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>*/}
     </div>
   );
 }
 
-export default App;
+/* function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+} */
+
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('RJ name should be in app', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+ 
+  const nameElement = getByText(/Rudy Jauregui/i);
+  expect(nameElement).toBeInTheDocument();
 });
